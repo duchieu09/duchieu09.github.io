221ecf3f64071426bfddc8ab9c69158db98d3761
hieu bui <buiduchieu517@gmail.com>
2022-03-12 12:54:23 +0700

    up date

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    const typed = new Typed(".auto-input",{
+       strings: ["Bùi Đức Hiếu","Web Developer","UI Designer"],
+       typedSpeed: 100,
+       backSpeed: 100,
+       loop:true,
+    })
+})
+ 
